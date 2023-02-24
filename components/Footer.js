4d5eff16db922df190e573d8263a4649e1e3a72b
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-02-24 21:23:15 +0530

    added privacy and contact page

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,33 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import styles from '../styles/Home.module.css'
 
 const Footer = () => {
   return (
-      <footer className={styles.footer}>
-          
-              Powered by 
-              aicodebuddy.com
+    <footer className={styles.footer}>
+      <div className='w-full flex items-center justify-center'>
+        <div className='flex items-center space-x-2 px-5'>
+          <Link href={"/privacy-policy"}>
+            privacy and policy
+          </Link>
+          <Link href={"/contact"}>
+            contact
+          </Link>
+        </div>
 
-      </footer>
+        <div className="flex items-center space-x-2">
+          <span>powered by</span>
+          <Image
+            src={"/robot.png"}
+            width={20}
+            height={20}
+            alt="menu"
+          />
+          <span>aicodebuddy.com</span></div>
+
+      </div>
+    </footer>
   )
 }
 
---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='min-h-screen'>
+          <h1>Contact us :</h1>
+          <p>mail at ms8460149@gmail.com</p>
+    </div>
+  )
+}
+
+export default Contact
