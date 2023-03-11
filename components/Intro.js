d67ed7e43362d151f04f722c73e9b9e09f26a768
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-03-12 02:37:08 +0530

    changed h1 and p size

--- components/Intro.js
+++ components/Intro.js
@@ -6,10 +6,10 @@ const Intro = ({ tagline = "Add comments into your code." }) => {
     return (
         <Fade bottom>
         <div>
-            <h1 className="text-4xl md:text-6xl text-center mt-10 md:mt-16 font-bold">
-                Welcome to <span className='text-blue-600'>aiCodeBuddy.com</span>
+            <h1 className="text-3xl md:text-6xl text-center mt-10 md:mt-16 font-bold">
+                Welcome to <span className='text-blue-600'>AiCodeBuddy.com</span>
             </h1>
-            <p className="text-center mt-5 text-2xl md:text-3xl">
+            <p className="text-center mt-5 text-lg md:text-3xl">
                 {tagline}
             </p>
             <FeaturesTag/>
