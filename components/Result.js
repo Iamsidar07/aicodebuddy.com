cd4618ceb3c8656ff132bae31ac6a41fcd70ee79
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-03-12 02:28:24 +0530

    Changed mt-9 to mt-5

--- components/Result.js
+++ components/Result.js
@@ -15,7 +15,7 @@ const Result = ({result}) => {
         </div>
           <FiCopy color='white' size={20} className="absolute top-3 right-3 cursor-pointer" onClick={(e) => copyToClipboard(result)} />
           <div
-          className="mr-1 mt-9 break-words"
+          className="mr-1 mt-5 break-words"
           dangerouslySetInnerHTML={{ __html: result.replaceAll('\n', '<br/>') }}
           />
       </div>
