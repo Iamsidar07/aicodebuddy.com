038907bc01e55e30bc9101505b0888fe7578eadc
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-03-19 00:21:18 +0530

    changed loader

--- components/Loading.js
+++ components/Loading.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import Lottie from 'react-lottie';
 import animationUrl1 from "../public/fire.json"
-import animationUrl2 from "../public/running-dog-3.json"
+import animationUrl2 from "../public/running-dog-2.json"
 const Loading = () => {
 
 
   const defaultOptions = {
     loop: true,
     autoplay: true,
-    animationData:animationUrl2,
+    animationData:animationUrl1,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice"
     }
