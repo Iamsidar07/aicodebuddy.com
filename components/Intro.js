aaa249b31e8e1c79c04c0ece1555526df3666b6a
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-03-19 11:39:18 +0530

    Update Intro.js

--- components/Intro.js
+++ components/Intro.js
@@ -6,8 +6,8 @@ const Intro = ({ tagline = "Add comments into your code." }) => {
     return (
         <Fade bottom>
         <div>
-            <h1 className="text-5xl md:text-7xl text-center mt-10 md:mt-16 font-extrabold">
-                    Welcome to <span className='text-gradient'>AiCodeBuddy.com</span>
+            <h1 className="text-3xl md:text-7xl text-center mt-10 md:mt-16 font-extrabold">
+                    Welcome to <span className='text-gradient text-4xl'>AiCodeBuddy.com</span>
             </h1>
                 <p className="text-center mt-5 text-lg md:text-3xl text-[#D0DFFF]">
                 {tagline}
